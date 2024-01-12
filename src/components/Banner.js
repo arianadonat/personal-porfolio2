6aaed64348f74792b678ec4fd56dd7c210b439a5
ariana <128388621+arianadonat@users.noreply.github.com>
2024-01-11 21:48:53 -0300

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -10,7 +10,7 @@ export const Banner = () => {
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [ setIndex] = useState(1);
-  const toRotate = [ "Web Developer", "Web Designer", "UI/UX Designer" ];
+  const toRotate = [ "Web Developer", "Web Designer" ];
   const period = 2000;
 
   useEffect(() => {
